94d0d2d23128ebe49678be22f579fdda34936766
gabrielromagnoli1987 <gabrielromagnoli1987@gmail.com>
2015-05-08 18:29:51 -0300

    se actualiza el settings para agregar la app account y el views de la misma

--- desafio/apps/account/views.py
+++ desafio/apps/account/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
-# Create your views here.
+from account.forms import UserForm
+
+
+def register(request):
+    
+    registered = False
+        
+    if request.method == 'POST':        
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():            
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()            
+            registered = True
+        else:
+            print user_form.errors    
+    else:
+        user_form = UserForm()
+            
+    return render(request, 'account/register.html', {'user_form': user_form, 'registered': registered} )
