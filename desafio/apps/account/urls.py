196bea50b2c8355880586e829bdd84d7da65cd32
gabriel <gabrielromagnoli1987@gmail.com>
2015-05-08 22:28:00 -0300

    se agrega el register (signup), el login y el logout

--- desafio/apps/account/urls.py
+++ desafio/apps/account/urls.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import *
 
-urlpatterns = patterns('',                       
-    url('^', include('django.contrib.auth.urls')),    
+from account import views
+
+
+urlpatterns = patterns('',
+    url(r'^register/$', views.register, name='register'),
+    
+    url(r'^login/$', login, {'template_name': 'account/login.html'}, name='login'),    
+    url(r'^logout/$', logout, name='logout'),
 )
